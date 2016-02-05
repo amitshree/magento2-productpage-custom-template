4579cbb95721fc27172e4c0ea2921a41e8db0085
Amit <amit103065@gmail.com>
2016-02-05 10:41:24 +0530

    initial commit

---
+++ app/code/Lapisbard/Catalog/Block/Product/View/Extra.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amit
+ * Date: 27/1/16
+ * Time: 11:21 AM
+ */
+namespace Lapisbard\Catalog\Block\Product\View;
+
+use Magento\Catalog\Block\Product\AbstractProduct;
+
+class Extra extends AbstractProduct
+{
+    public function getSomething()
+    {
+        return 'something';
+}
+
+}
---
+++ app/code/Lapisbard/Catalog/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amit
+ * Date: 19/1/16
+ * Time: 4:16 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Lapisbard_Catalog',
+        __DIR__
+    );
